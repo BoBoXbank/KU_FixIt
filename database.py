5b7a85bae5c08ce4761df66fa679c1ad113f6844
Kanokporn Prim <pxrxim@gmail.com>
2026-02-21 19:00:52 +0700

    เพิ่ม  cssเพื่อความสระสลวย

--- database.py
+++ database.py
@@ -10,6 +10,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = int(os.getenv("DB_PORT", 4000)) 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+
 DB_NAME = os.getenv("DB_NAME")
 DB_CA = os.getenv("DB_CA")
 
