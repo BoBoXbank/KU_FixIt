55e94198af429ba6144158d732efddc574406e48
Paphakorn Chandee <papakon805@gmail.com>
2026-03-12 17:34:14 +0700

    Feat add profile image optimizer

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from technician_routes import technician_bp
 app = Flask(__name__)
 app.secret_key = "KU_Super_Secret_Key_2026"  # กุญแจลับสำหรับ Session
 
+# เพิ่มขีดจำกัดขนาดไฟล์อัปโหลดสูงสุดเป็น 16MB เพื่อรองรับรูปภาพความละเอียดสูง
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 
+
 app.register_blueprint(user_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(technician_bp)
